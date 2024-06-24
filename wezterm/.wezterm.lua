cd883185afd47bef86440f50f51c9a13983c7ddb
Abijit Suresh <30644549+abijitsuresh@users.noreply.github.com>
2024-06-23 23:02:37 -0400

    updated theme for wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,15 +6,16 @@ local config = wezterm.config_builder()
 
 function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
-    return "Everforest Dark Medium (Gogh)" -- "Catppuccin Mocha" "Everforest Dark Medium (Gogh)" "rose-pine" "rose-pine-moon"
+    return "Catppuccin Mocha" -- "Catppuccin Mocha" "Everforest Dark Medium (Gogh)" "rose-pine" "rose-pine-moon"
   else
     return "Everforest Light Soft (Gogh)" -- "Catppuccin Mocha" "Everforest Light Soft (Gogh)"
   end
 end
 
 -- This is where you actually apply your config choices
-
+--config.color_scheme = 'rose-pine-moon'
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+--config.color_scheme = 'Everforest Dark Medium (Gogh)'
 -- config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 config.font = wezterm.font 'Hack Nerd Font'
